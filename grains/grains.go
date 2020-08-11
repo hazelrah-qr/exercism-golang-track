827ea922eaa149fcb5c81f7ad14d289bea20b119
hazelrah-qr <martin@qrubba.se>
2020-08-11 23:27:38 +0200

    Grains reworked based on feedback

--- grains/grains.go
+++ grains/grains.go
@@ -4,20 +4,14 @@ import "errors"
 
 // Square number of grains on the square
 func Square(grains int) (uint64, error) {
-	if grains <= 0 {
-		return 0, errors.New("Error")
+	if grains <= 0 || grains > 64 {
+		return 0, errors.New("Error grains out of range")
 	}
 
-	if grains > 64 {
-		return 0, errors.New("Too large number")
-	}
-	result := uint64(1) << (grains - 1)
-	return result, nil
+	return 1 << (grains - 1), nil
 }
 
 // Total number of grains on a chessboard
 func Total() uint64 {
-	result, _ := Square(64)
-	result = uint64(result)<<1 - 1
-	return result
+	return (1 << 64) - 1
 }
